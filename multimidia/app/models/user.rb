1808c03d643708b26abdd3bddad25bc8650d7445
Danilo Caetano <dcms.pe@gmail.com>
2015-07-03 17:45:13 -0300

    Creation of class user with the unit tests.

--- multimidia/app/models/user.rb
+++ multimidia/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :email, email_format:{ messagem: "The email is not valid."}
+	validates :password, length:{ minimum:8, maximum:16 }
+	validates :name, presence:true, length:{maximum:20}
 end
--- multimidia/test/models/user_test.rb
+++ multimidia/test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User must be not null" do
+  	user = User.new
+    assert !user.nil?
+  end
+
+  test "User must hava an valid e-mail" do
+  	user = User.create(email: "dcms.pe@gmail.com", password:"bankaiic", name:"Danilo")
+    assert user.valid?
+  end
+
+  test "User must detect an invalid e-mail" do 
+  	user = User.create(email: "applepie", password:"bankaiic", name:"Danilo")
+  	assert !user.valid?
+  end
+
+
+  test "User cannot have a blank email" do
+    user = User.create(email: "", password:"bankaiic", name:"Danilo")
+    assert !user.valid?
+  end
+
+  test "User must have at least 8 letters inside his password" do
+    user = User.create(email: "dcms.pe@gmail.com", password: "bankaiic", name:"Danilo")
+    assert user.valid?
+  end
+
+  test "User must have at maximum 16 letters inside his password" do
+    user = User.create(email: "dcms.pe@gmail.com", password: "bankaibankaibank", name:"Danilo")
+    assert user.valid?
+  end
+
+  test "Name can not be blank" do
+    user = User.create(email: "dcms.pe@gmail.com", password: "bankaiic", name:"")
+    assert !user.valid?
+  end
+
+  test "Name can just have 20 letters" do
+    user = User.create(email: "dcms.pe@gmail.com", password: "bankaiic", name:"Danilo Caetano Matia")
+    assert user.valid?
+  end
+
 end
